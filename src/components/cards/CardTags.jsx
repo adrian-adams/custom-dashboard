5499bf9b2d489b6e22af52181868be393a5ed200
helpimblind <adrian.adamsgit@outlook.com>
2025-10-29 17:13:58 +0200

    Update CardTags.jsx

--- src/components/cards/CardTags.jsx
+++ src/components/cards/CardTags.jsx
@@ -22,7 +22,7 @@ const cardTagList = [
   "Security", "SEO", "Portfolio", "Template", "Theme", "Media", 
   "Testing", "DevOps", "Cloud", "Game Dev", "Git", "PDF", "JSON", 
   "AI", "Hosting", "Google", "FEM", "Image Library", "Icon Library", 
-  "UTM", "Bitly", "CSV", "BE", "FE", "Fullstack", "Images"
+  "UTM", "Bitly", "CSV", "BE", "FE", "Fullstack", "Images", "GIF"
 ]
 
 const sortedList = cardTagList.sort();
